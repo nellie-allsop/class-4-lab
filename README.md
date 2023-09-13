308da340595809c72bc9553d35063bbcab1d7e84
Nellie Allsop <nellie.allsop@gmail.com>
2023-09-13 20:47:14 +0100

    Change these at end of day 13/09/23

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # class-4-lab
+
+Colours = https://coolors.co/3a2e39-1e555c-f4d8cd-edb183-f15152
+
+create three separate javascript files
---
+++ name.js
@@ -0,0 +1,7 @@
+let username = prompt("Howdy, what's your name weary traveller?");
+
+if (username ≠ null)
+
+document.write(message);
+
+message = "Hiya " + username + "! "
