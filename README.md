e3b0a1b82e429bbd8f83d8cb22e031e55450d09a
Nellie Allsop <nellie.allsop@gmail.com>
2023-09-17 22:01:24 +0100

    Weekend update

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Colours = https://coolors.co/3a2e39-1e555c-f4d8cd-edb183-f15152
 
 Credit to https://www.youtube.com/watch?v=8YJTOse5nCc for the code to arrange the three film posters 🙌
 
+id in html
+
+Revels. jpg img
+
+class = "test"
--- function.js
+++ function.js
@@ -25,3 +25,17 @@ function changebackgroundcolor()
 {
     document.body.style.backgroundColor = '#3A2E39';
     }
+
+function getRevels() {
+
+    let rating = prompt("And finally, how many bags of revels would you like (for your movie snack)? WARNING there's only 10 bags in stock!");
+
+while (!(rating > 0 && rating <=5)) {
+        rating = prompt("Greedy! That's too many!")
+    }
+
+for (let i = 0; i < rating; i++) {
+    document.write("<img src='./Images/Revels.jpg'/>");
+    }
+
+}
